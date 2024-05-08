1e41e2c46d3755e671e9ea4e6c239763b7779aca
marcohoucheng <marcohoucheng@gmail.com>
2024-05-08 12:05:49 +0200

    Created structure of algorithm implementation

---
+++ algorithm.rs
@@ -0,0 +1,8 @@
+// Read the 2 csv
+// Find number of unique service types
+// Add a node to connect to all locations for each service type
+// Run Dijsktra from each of these nodes created
+// - With Fibonacci Heap
+// - Mark visited nodes (array of bool of length unique service types)
+// - Stop after 15 minutes
+// Return nodes where visited array is all true
